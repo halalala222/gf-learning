acdd14b02869350abca75baa83d0859cb25b1d36
halalala222 <1741196223@qq.com>
2023-04-08 11:11:21 +0800

    feat:define Rep & Res struct

---
+++ api/hello/v1/ncu_home.go
@@ -0,0 +1,14 @@
+package v1
+
+import "github.com/gogf/gf/v2/frame/g"
+
+type NcuHomeRep struct {
+	g.Meta `path:"/ncuhome" tags:"NcuHomeService" method:"post"`
+	Name   string `v:"required"`
+	Group  string `v:"required"`
+	Sex    string `v:"required"`
+}
+
+type NcuHomeRes struct {
+	Id int64 `json:"id"`
+}
