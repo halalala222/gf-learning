163ef9c08232f31ef2c0571a7cec6f84abf52790
halalala222 <1741196223@qq.com>
2023-04-12 20:19:18 +0800

    feat:use jwt auth middleware

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"context"
 	"github.com/halalala222/gf-learning/internal/controller/ncu_home"
+	"github.com/halalala222/gf-learning/internal/service"
 
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
@@ -17,7 +18,7 @@ var (
 		Func: func(ctx context.Context, parser *gcmd.Parser) (err error) {
 			s := g.Server()
 			s.Group("/", func(group *ghttp.RouterGroup) {
-				group.Middleware(ghttp.MiddlewareHandlerResponse)
+				group.Middleware(ghttp.MiddlewareHandlerResponse, service.Middleware().Auth)
 				group.Bind(
 					ncu_home.New(),
 				)
