9c908954d143fba077e1dd41c1750179c17a3b3e
halalala222 <1741196223@qq.com>
2023-04-13 16:31:41 +0800

    feat:add walletLogin req & res model

--- api/v1/user.go
+++ api/v1/user.go
@@ -13,3 +13,15 @@ type LoginRes struct {
 	Token  string    `json:"token"`
 	Expire time.Time `json:"expire"`
 }
+
+type WalletLoginReq struct {
+	g.Meta    `path:"/wallet/login" method:"POST"`
+	Signature string `v:"required" json:"signature"`
+	Address   string `v:"required" json:"address"`
+	Challenge string `v:"required" json:"challenge"`
+}
+
+type WalletLoginRes struct {
+	Token  string    `json:"token"`
+	Expire time.Time `json:"expire"`
+}
