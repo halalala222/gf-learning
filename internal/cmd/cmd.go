7346c0fa703e0f68b9fb41bf8b87ef8f4b7d71d6
halalala222 <1741196223@qq.com>
2023-04-13 16:42:21 +0800

    fix:use controller wallet.New()

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gogf/gf/v2/os/gcmd"
 	"github.com/halalala222/gf-learning/internal/controller/ncu_home"
 	"github.com/halalala222/gf-learning/internal/controller/user"
+	"github.com/halalala222/gf-learning/internal/controller/wallet"
 	"github.com/halalala222/gf-learning/internal/service"
 )
 
@@ -20,7 +21,8 @@ var (
 			s.Group("/", func(group *ghttp.RouterGroup) {
 				group.Middleware(ghttp.MiddlewareHandlerResponse)
 				group.Bind(
-					user.New(),
+					user.New().Login,
+					wallet.New().Login,
 				)
 				group.Group("/", func(group *ghttp.RouterGroup) {
 					group.Middleware(ghttp.MiddlewareHandlerResponse, service.Middleware().Auth)
@@ -28,6 +30,7 @@ var (
 						"/ncuhome": ncu_home.New().CreatePerson,
 					})
 				})
+
 			})
 			s.Run()
 			return nil
